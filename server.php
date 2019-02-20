0ddc96ca2668d056e95ee0d7a7fa9b02c72b1da4
1808234 <1808234@rgu.ac.uk>
2019-02-20 10:44:51 +0000

    Project CMM004

--- server.php
+++ server.php
@@ -9,31 +9,35 @@ $db= mysqli_connect('CSDM-WEBDEV','1809441','1809441','db1809441_trade');
 
 //register user
 if(isset($_POST['userRej'])){
+
+    $username= mysqli_real_escape_string($db,$_POST['username']);
+    $email = mysqli_real_escape_string($db,$_POST['email']);
+    $password_1= mysqli_real_escape_string($db,$_POST['password']);
+    $password_2 = mysqli_real_escape_string($db,$_POST['password1']);
+
     $f_name= mysqli_real_escape_string($db,$_POST['1name']);
     $L_name= mysqli_real_escape_string($db,$_POST['u2name']);
     $add_1= mysqli_real_escape_string($db,$_POST['add1']);
     $add_2= mysqli_real_escape_string($db,$_POST['add2']);
     $city= mysqli_real_escape_string($db,$_POST['city']);
     $postcode= mysqli_real_escape_string($db,$_POST['postcode']);
-    $username= mysqli_real_escape_string($db,$_POST['username']);
-    $email = mysqli_real_escape_string($db,$_POST['email']);
-    $password_1= mysqli_real_escape_string($db,$_POST['password']);
-    $password_2 = mysqli_real_escape_string($db,$_POST['password1']);
 
     //to make sure all or most of the places are filled correctly
-    if(empty($f_name)){ array_push($error,"First name is required");}
-    if(empty($L_name)){ array_push($error,"Last name is required");}
-    if(empty($add_1)){ array_push($error,"the first line is required");}
-    if(empty($city)){ array_push($error,"city name is required");}
-    if(empty($postcode)){ array_push($error,"postcode is required");}
     if(empty($username)){ array_push($error,"username is required");}
     if(empty($email)){ array_push($error,"email is required");}
     if(empty($password_1)){ array_push($error,"Password is required");}
     if($password_1 != $password_2){array_push($error,"the password do not match");}
 
+    if(empty($f_name)){ array_push($error,"First name is required");}
+    if(empty($L_name)){ array_push($error,"Last name is required");}
+    if(empty($add_1)){ array_push($error,"the first line is required");}
+    if(empty($city)){ array_push($error,"city name is required");}
+    if(empty($postcode)){ array_push($error,"postcode is required");}
+
+
     // check the data base to make sure the user does not already exist with
     //username/ or password and email
-    $user_check= " SELECT* FROM user WHERE (username='$username' OR email= '$email') LIMIT 1";
+    $user_check= " SELECT * FROM login WHERE (username='$username' OR email= '$email') LIMIT 1";
     $result= mysqli_query($db,$user_check);
     $user= mysqli_fetch_assoc($result);
 
@@ -50,15 +54,32 @@ if(isset($_POST['userRej'])){
     if(empty($error)){
 
         $password= md5($password_1); // encrypt the password be for save it in our database
-        echo " we done it ";
 
-       $put= "INSERT INTO test (1name, u2name, add1, add2, city, postcode, username, email, password)
-VALUES ('$f_name','$L_name','$add_1','$add_2','$city','$postcode','$username','$email','$password')";
-        echo " we done it 1";
+
+
+       $put= "INSERT INTO login (username,email,password) VALUES ('$username','$email','$password')";
+        mysqli_query($db,$put);
+        $_SESSION['username']= $username;
+        $_SESSION['success'] = "you are now logged in Successfully !";
+        header('location: index.php/?do it');
+        echo " we done it 3";
         header('Location: /ourProjecrtCM0004?case1');
+        exit();
 
-     //   $put= "INSERT INTO table login (username,email,password)
-       //         VALUES ('$username','$email','$password')";
+
+        $put2= "SELECT id_user FROM login WHERE username=$username";
+
+        $log_id = mysqli_insert_id($db);
+
+        echo "$log_id";
+        /*
+        $_SESSION['username']= getUserById($log_id);
+
+
+        $put2= "SELECT id_user FROM login WHERE id_log=$log_id";
+
+        /** @var TYPE_NAME $f_name */
+        $put= "INSERT INTO user(user_id,firstname,lastname,address1,address2,city,postcode) VALUES ('$log_id','$f_name','$L_name','$add_1','$add_2','$city','$postcode')";
         echo " we done it 2";
        // $put= "INSERT INTO table login(username,email,password) VALUES ('$username','$email','$password')";
         mysqli_query($db,$put);
